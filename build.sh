0e2b7ab627cdf6e2310160bec229193c0a9acdd4
Felipe Alvarado <118953310+Oppais2001@users.noreply.github.com>
2026-08-07 18:42:07 -0400

    Primera subida de datos locales a la nube de prueba en json

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
+python manage.py loaddata datos.json
 python manage.py migrate
 python manage.py createsuperuser --noinput || true
