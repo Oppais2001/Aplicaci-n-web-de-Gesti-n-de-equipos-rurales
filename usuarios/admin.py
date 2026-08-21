3100e34d5862f76717644e98583fa43bc8340055
Felipe Alvarado <118953310+Oppais2001@users.noreply.github.com>
2026-08-21 02:19:12 -0400

    Avances de restricciones a usuarios

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -6,4 +6,14 @@ from .models import Usuario
 @admin.register(Usuario)
 class UsuarioAdmin(UserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
     search_fields = ('username', 'email', 'first_name', 'last_name')
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': ('username', 'email', 'password1', 'password2', 'is_active'),
+            },
+        ),
+    )
