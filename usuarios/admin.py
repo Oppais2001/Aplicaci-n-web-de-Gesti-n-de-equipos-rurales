9f95a200af8c1c71ecc15faa9d74dff1264ac073
Felipe Alvarado <118953310+Oppais2001@users.noreply.github.com>
2026-08-21 01:35:34 -0400

    Corrigiendo verificacion de correo

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Usuario
 
-admin.site.register(Usuario)
+
+@admin.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
