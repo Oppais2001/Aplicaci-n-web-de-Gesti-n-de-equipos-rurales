aa82b74b8965d852587a99426730a0022ad2fa6e
Felipe <118953310+Oppais2001@users.noreply.github.com>
2026-09-11 00:13:30 -0300

    Actualizar admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from .models import Arbitro, Dirigente, Equipo, Jugador, Liga, RedSocial, Traspaso, Cancha, Partido, Torneo
 
+
+@admin.register(Jugador)
+class JugadorAdmin(admin.ModelAdmin):
+    search_fields = ['nombre', 'rut']
+
+
 admin.site.register(Arbitro)
 admin.site.register(Dirigente)
 admin.site.register(Equipo)
-admin.site.register(Jugador)
 admin.site.register(Liga)
 admin.site.register(RedSocial)
 admin.site.register(Traspaso)
